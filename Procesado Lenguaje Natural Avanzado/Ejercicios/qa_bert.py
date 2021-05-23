d13a6ffa9f6ed1154018c818f5df7c5b6d94f989
Jorge Vila Tomás <vila_tronik@hotmail.com>
2021-05-23 18:26:29 +0200

    Erased useless comments

--- Procesado Lenguaje Natural Avanzado/Ejercicios/qa_bert.py
+++ Procesado Lenguaje Natural Avanzado/Ejercicios/qa_bert.py
@@ -120,7 +120,6 @@ if __name__ == "__main__":
     QUESTION = 'When were the Normans in Normandy?'
     PARAGRAPH = "The Normans were the people who in the 10th and 11th centuries gave their name to Normandy, a region in France. They were descended from Norse (\"Norman\" comes from \"Norseman\") raiders and pirates from Denmark, Iceland and Norway who, under their leader Rollo, agreed to swear fealty to King Charles III of West Francia. Through generations of assimilation and mixing with the native Frankish and Roman-Gaulish populations, their descendants would gradually merge with the Carolingian-based cultures of West Francia. The distinct cultural and ethnic identity of the Normans emerged initially in the first half of the 10th century, and it continued to evolve over the succeeding centuries."
     QAmodel = QABert()
-    # a, b = QAmodel.strings2tensors(QUESTION, PARAGRAPH)
     answer = QAmodel.predict(QUESTION, PARAGRAPH)
     
     print(f'QUESTION: {QUESTION}\n')
